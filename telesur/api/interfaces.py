eeb586ccfbb0bcedbc3f73becf4cba026212f8f0
Joaquín Rosales <globojorro@gmail.com>
2011-12-28 11:24:23 -0600

    Se agregan las interfaces del paquete

--- telesur/api/interfaces.py
+++ telesur/api/interfaces.py
@@ -1,7 +1,24 @@
 # -*- coding: utf-8 -*-
 
+from zope.interface import Interface
 from plone.theme.interfaces import IDefaultPloneLayer
+
+
+class IPortalAPI(Interface):
+    """ Interfaz genérica para APis """
 
 
 class ITelesurAPILayer(IDefaultPloneLayer):
     """ Default browser layer for telesur.api """
+
+
+class IPortalAPIQuerySchema(Interface):
+    """ Interfaz para describir los campos de búsqueda """
+
+
+class IVideoAPIQuerySchema(Interface):
+    """ Interfaz para describir los campos de búsqueda """
+
+
+class IAPIViewlet(Interface):
+    """ Interfaz para viewlets del API """
